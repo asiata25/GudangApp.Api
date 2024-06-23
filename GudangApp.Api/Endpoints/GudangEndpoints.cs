a945064b60c11eb55d18ed75fe6af46612450c7a
Mohamad Lutfi Khoirudin <asiatakh25@gmail.com>
2024-06-23 20:27:41 +0700

    build: define endpoint for gudang

---
+++ GudangApp.Api/Endpoints/GudangEndpoints.cs
@@ -0,0 +1,25 @@
+﻿namespace GudangApp.Api.Endpoints;
+
+public static class GudangEndpoints
+{
+  public static RouteGroupBuilder MapGudangEndpoints(this WebApplication app) {
+    var group = app.MapGroup("api/v1/gudangs");
+
+    // GET /api/v1/gudangs
+    group.MapGet("/", () => Results.Ok("semua gudang"));
+
+    // POST /api/v1/gudangs
+    group.MapPost("/", () => Results.Ok("membuat gudang"));
+
+    // GET /api/v1/gudangs/:id
+    group.MapGet("/{id}", (string id) => Results.Ok("id gudang"));
+
+    // PUT /api/v1/gudangs/:id
+    group.MapPut("/{id}", (string id) => Results.Ok("put gudang"));
+
+    // DELETE /api/v1/gudangs/:id
+    group.MapDelete("/{id}", (string id) => Results.Ok("delete gudang"));
+
+    return group;
+  }
+}
